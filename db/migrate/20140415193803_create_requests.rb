e750927ef438b48ead8fa9909481f69d0b26bf35
jaybobo <info@goodproduce.net>
2014-04-15 20:28:31 -0500

    finished parser

--- db/migrate/20140415193803_create_requests.rb
+++ db/migrate/20140415193803_create_requests.rb
@@ -1,4 +1,17 @@
 class CreateRequests < ActiveRecord::Migration
   def change
+  	create_table :requests do |t|
+  		t.string :user_city
+  		t.string :user_state
+  		t.string :user_msa
+  		t.string :user_type
+  		t.string :request_id
+  		t.datetime :request_time
+  		t.string :form_name
+  		t.string :drug_name
+  		t.date :month_part
+  		t.string :territory
+  		t.string :district_number
+  	end
   end
 end
--- db/parser.rb
+++ db/parser.rb
@@ -20,22 +20,53 @@ def strip_header_strings(array_of_strings)
 	end
 end
 
-def transform_remaining_double_strings(array_of_strings)
-	array_of_strings.each do |array|
-		array.map do |el|
-			if el == array[1] || el == array[2]
-				el.gsub!(/^\"|\"?$/, '')
-			else
-				el
-			end
+# def transform_remaining_double_strings(array_of_strings)
+# 	array_of_strings.each do |array|
+# 		array.map do |el|
+# 			if el == array[1] || el == array[2]
+# 				el.gsub!(/^\"|\"?$/, '')
+# 			else
+# 				el
+# 			end
+# 		end
+# 	end
+# end
+
+def strip_each_array(array)
+	array.each do |inner_array|
+		inner_array.map do |i|
+			i.gsub!(/^\"|\"?$/, '')
 		end
 	end
 end
 
+def zipped(headers, cleaned_data)
+	cleaned_data.inject(Array.new) do |memo, array|
+		memo << headers.zip(array)
+	end
+end
 
 
+def change_arrays_to_hashes(array_of_arrays)
+	array_of_arrays.map do |array|
+		Hash[array]
+	end
+end
+
+one = parser_one(FILE2)
+
+headers = strip_header_strings(one[0])
+
+cleaned_data =  strip_each_array(one[1..-1].pop(5)) 
+
+key_value_array = zipped(headers, cleaned_data)
+
+change_arrays_to_hashes(key_value_array).each do |hash|
+	Request.new(hash)
+end
 
 #ALL STRINGS CLEANED, READY TO BE ZIPPED TO HEADERS!
-one = parser_one(FILE1)
-p strip_header_strings(one[0]) #headers alone
-p transform_remaining_double_strings(one[1..-1]) #rest of data, 
+
+# p strip_header_strings(one[0]) #headers alone
+#p transform_remaining_double_strings(one[1..-1].pop(5)) #rest of data, 
+
