15641bfe198f2267eb9b0f8dfb8909b26fe9e443
Ron Gierlach <rongierlach@yahoo.com>
2014-04-18 03:27:54 -0500

    The views so far--to Reports

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,12 +10,26 @@ end
 
 # ---------- TESTING ----------
 
-get '/user' do
-
-  erb :'users/index'
+get '/dashboard' do
+  erb :'dashboard'
 end
 
-get '/login' do 
+get '/log_in' do 
   erb :log_in
 end
 
+get '/reports' do
+  erb :report
+end
+
+get '/training' do 
+  erb :training
+end
+
+get '/outreach' do
+  erb :outreach
+end
+
+get '/help' do
+  erb :help
+end
